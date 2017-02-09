d13c22a35d8145dbdaa2637a7144acca6e45a97c
Constantine <business@netweightit.com>
2017-02-08 21:58:09 -0800

    mongoose

--- app/app.js
+++ app/app.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon'); 
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+var mongoose = require('mongoose');  
+mongoose.connect('mongodb://localhost/dafuckmyli_ve');
+
+// mongoose.connect('mongodb://heroku_mfstx9tp:qwerty123456789@ds143588.mlab.com:43588/heroku_mfstx9tp');
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+
+
+var index  = require('./routes/index');
+var player = require('./routes/player');
 
 var app = express();
 
@@ -22,8 +37,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+app.use(function(req, res, next){
+	req.db = db;
+	next();
+});
+
+
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/player', player);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
