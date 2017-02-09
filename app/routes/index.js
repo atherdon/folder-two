7d4ea56c3feb2f247c312ff824c4e2197b14325f
Constantine <business@netweightit.com>
2017-02-09 00:41:07 -0800

    re-arrange views

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,10 +4,85 @@ var router   = express.Router();
 
 const util   = require('util');
 
-var http = require('http');
+// var http = require('http');
 
 //models
 var Video    = require('../models/video');
+
+
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.post('/insert', function(req, res){
+
+	// console.log(util.inspect( req.body, false, null ));
+
+	//@todo remove startB & endB from form elements
+
+	var url   = req.body.url;
+	
+	var start = req.body.start;
+	var end   = req.body.end;
+	var title = req.body.title;
+	var desc  = req.body.desc;
+	var step  = req.body.step; 
+	
+
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match  = url.match(regExp);
+    var slug   = (match&&match[7].length==11)? match[7] : false;
+
+	var value = new Video({
+	     title : title,
+	     url   : url,
+	     desc  : desc,
+	     start : start,
+	     end   : end,
+	     step  : step,
+	     slug  : slug,
+	});
+
+	// console.log( value );
+
+	value.save().then(function(docs){
+
+		// console.log( docs );
+		// console.log( docs._id );
+
+		docs.linka = '/edit/' + docs._id + ''; //@todo change this later and move to layout template
+		// docs.linka = '/edit/?gid=' + docs._id + '';
+
+		// res.render('player', docs);
+
+
+		res.render('player3', docs);
+
+	});
+
+
+});
+
+
+// this controller relates to show cases pages.
+// @todo readme later to more simple and appropriate
+router.get('/example', function(req, res){
+
+	var object = {
+		title    : false,
+		subtitle : false,
+		link     : false,
+		body     : false,
+	}
+
+	res.render('example1', object);
+})
 
 
 router.get('/mongotest', function(req, res, next) {
@@ -46,7 +121,7 @@ router.get('/mongotest', function(req, res, next) {
 
 		console.log(docs);
 		console.log('Video created!');
-		
+
 	});
 
 
@@ -73,12 +148,13 @@ router.get('/mongotest', function(req, res, next) {
 
 });
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
-
 module.exports = router;
+
+
+// Fancy console.log
+function output (err, data) {
+  console.dir (err || data, {
+    depth: null,
+    colors: true
+  });
+}
--- app/routes/player.js
+++ app/routes/player.js
@@ -1,9 +1,108 @@
-var express = require('express');
-var router = express.Router();
+var express  = require('express');
+var router   = express.Router();
+
+
+const util   = require('util');
+
+// var http = require('http');
+
+//models
+var Video    = require('../models/video');
+
 
 /* GET player functionality. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+
+
+router.get('/:id', function(req, res){
+
+	Video.findById( req.params.id, function(err, video) {
+
+		if (err) 
+			res.render('empty', { title: 'Error' });
+				// res.send(err);
+
+			res.render('player-only', video );
+
+	});
+
+
+
 });
 
+
+router.get('/edit/:id/', function(req, res) {
+		
+	// console.log(util.inspect( req.params, false, null ));	
+
+ 	Video.findById( req.params.id, function(err, video) {
+
+		if (err) 
+			res.render('empty', { title: 'Error' });
+				// res.send(err);
+
+			console.log(video)	;
+
+			// video.duration = req.params.duration;
+
+			res.render('edit', video ); //@todo change to sending maybe two objects, or calculate duration by youtube on page instead
+
+	
+
+			
+
+	});
+
+});	
+
+router.post('/update/', function(req, res){
+
+	//@todo receive only update form values
+	// console.log(util.inspect( req.params, false, null ));	
+	console.log(util.inspect( req.body, false, null ));
+	console.log( req.body.id );
+
+ 	Video.findById( req.body.id, function(err, video) {
+
+		if (err) 
+			res.render('empty', { title: 'Error' });
+
+
+		console.log( req.body.start );
+		console.log( req.body.end );
+		// console.log(video)	;
+
+		video.start = req.body.start;
+		video.end   = req.body.end;
+
+		video.save(function (err) {
+			if (err) throw err;
+
+			// console.log( 'updated' );
+		});
+
+		//@todo add thank you for using our service
+		// res.redirect("index"); //@todo test title
+		// res.render('index', { title: 'CutStream | Start & End point for your video' });
+
+	});
+
+
+});
+
+
+
+
 module.exports = router;
+
+
+// Fancy console.log
+function output (err, data) {
+  console.dir (err || data, {
+    depth: null,
+    colors: true
+  });
+}
